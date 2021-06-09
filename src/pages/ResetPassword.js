9fa343d4c4fca114ac5c03c19c8d3165378d7b15
jsjsjskjs <jsjsjskjs@gmail.com>
2021-06-09 15:46:56 +0900

    CSS EDIT_3

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -10,9 +10,9 @@ function ResetPassword() {
       <Header />
 
       {/*  Page content */}
-      <main className="flex-grow" id="signUp-BG">
+      <main className="flex-grow">
 
-        <section className="bg-gradient-to-b from-gray-100 to-white">
+        <section className="bg-gradient-to-b from-gray-100 to-white" id="signUp-BG">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
 
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -11,9 +11,9 @@ function SignUp() {
       <Header />
 
       {/*  Page content */}
-      <main className="flex-grow" id="signUp-BG">
+      <main className="flex-grow">
 
-        <section className="bg-gradient-to-b from-gray-100 to-white">
+        <section className="bg-gradient-to-b from-gray-100 to-white" id="signUp-BG">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
 
